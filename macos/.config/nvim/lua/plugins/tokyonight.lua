4c3c45c56217e1fc984b4cffd0e1baa6d3d1b3ee
Carlos Castro <carlos.castro@mongodb.com>
2024-10-03 14:49:58 +0100

    set default theme in nvim;

---
+++ macos/.config/nvim/lua/plugins/tokyonight.lua
@@ -0,0 +1,17 @@
+return {
+
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      style = "night",
+    },
+  },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+    },
+  },
+}
