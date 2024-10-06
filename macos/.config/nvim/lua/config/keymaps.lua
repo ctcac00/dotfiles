e9e119b08f2fd104f321507a65112061ca597acc
Carlos Castro <carlos.castro@mongodb.com>
2024-10-06 14:53:15 +0100

    update nvim config files;

--- macos/.config/nvim/lua/config/keymaps.lua
+++ macos/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "<leader>cL", function()
+  require("telescope.builtin").lsp_document_symbols({ symbols = "function" })
+end, { noremap = true, silent = true, desc = "Find Function" })
