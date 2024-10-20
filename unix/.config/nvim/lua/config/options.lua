3a0b52192f72cd121e3674b69658b53f9e7b2c96
Carlos Castro <carloscastro7@gmail.com>
2024-10-20 05:47:48 +0100

    use powershell on windows;

--- unix/.config/nvim/lua/config/options.lua
+++ unix/.config/nvim/lua/config/options.lua
@@ -2,3 +2,11 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.winbar = "%=%m %f"
+
+-- Check if the system is Windows and set PowerShell as the default shell
+if vim.loop.os_uname().sysname == "Windows_NT" then
+  vim.opt.shell = "pwsh"
+  vim.opt.shellcmdflag = "-NoProfile -Command"
+  vim.opt.shellquote = ""
+  vim.opt.shellxquote = ""
+end
