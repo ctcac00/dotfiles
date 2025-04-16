d91a607dcd1759ab9171864962dd98519337b83b
Carlos Castro <carloscastro7@gmail.com>
2025-04-16 13:59:55 +0100

    remove deprecated shortcut;

--- unix/.config/nvim/lua/config/keymaps.lua
+++ unix/.config/nvim/lua/config/keymaps.lua
@@ -1,9 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set("n", "<leader>cL", function()
-  require("telescope.builtin").lsp_document_symbols({ symbols = "function" })
-end, { noremap = true, silent = true, desc = "Find Function" })
 
 -- Resize window using <ctrl> arrow keys
 vim.keymap.set("n", "<A-Up>", "<cmd>resize +2<cr>", { noremap = true, silent = true, desc = "Increase Window Height" })
