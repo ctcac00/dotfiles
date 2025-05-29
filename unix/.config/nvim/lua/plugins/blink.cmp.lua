42b9332483d90e54b978aa97f27bc18bc7d4fd38
Carlos Castro <carloscastro7@gmail.com>
2025-05-29 07:40:40 +0100

    add source name to blink;

--- unix/.config/nvim/lua/plugins/blink.cmp.lua
+++ unix/.config/nvim/lua/plugins/blink.cmp.lua
@@ -4,11 +4,27 @@ return {
     completion = {
       menu = {
         border = "rounded",
+        draw = {
+          columns = {
+            { "kind_icon" },
+            { "label", "label_description", gap = 1 },
+            { "source_name", gap = 1 },
+          },
+        },
       },
       documentation = {
         window = {
           border = "rounded",
         },
+      },
+    },
+    sources = {
+      -- adding any nvim-cmp sources here will enable them
+      -- with blink.compat
+      compat = {
+        "avante_commands",
+        "avante_mentions",
+        "avante_files",
       },
     },
   },
