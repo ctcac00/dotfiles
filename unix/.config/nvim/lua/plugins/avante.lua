8c8427d3b18a470c382ac6866dcf530b86a9d38b
Carlos Castro <carloscastro7@gmail.com>
2025-05-29 10:43:24 +0100

    changed picker to fzf-lua due to an issue with avante and snacks.picker;

--- unix/.config/nvim/lua/plugins/avante.lua
+++ unix/.config/nvim/lua/plugins/avante.lua
@@ -12,9 +12,6 @@ return {
       temperature = 0,
       max_tokens = 4096,
     },
-    selector = {
-      provider = "snacks",
-    },
   },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
   build = "make",
@@ -24,33 +21,5 @@ return {
     "stevearc/dressing.nvim",
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
-    --- The below dependencies are optional,
-    "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
-    "echasnovski/mini.icons", -- or echasnovski/mini.icons
-    {
-      -- support for image pasting
-      "HakonHarnes/img-clip.nvim",
-      event = "VeryLazy",
-      opts = {
-        -- recommended settings
-        default = {
-          embed_image_as_base64 = false,
-          prompt_for_file_name = false,
-          drag_and_drop = {
-            insert_mode = true,
-          },
-          -- required for Windows users
-          use_absolute_path = true,
-        },
-      },
-    },
-    {
-      -- Make sure to set this up properly if you have lazy=true
-      "MeanderingProgrammer/render-markdown.nvim",
-      opts = {
-        file_types = { "markdown", "Avante" },
-      },
-      ft = { "markdown", "Avante" },
-    },
   },
 }
