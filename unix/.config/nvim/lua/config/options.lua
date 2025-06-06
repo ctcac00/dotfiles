8983f01afe887081392ad51fdee622f2aa128d9c
Carlos Castro <carlos.castro@mongodb.com>
2025-06-06 06:50:50 +0100

    update nvim settings;

--- unix/.config/nvim/lua/config/options.lua
+++ unix/.config/nvim/lua/config/options.lua
@@ -1,7 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.opt.winbar = "%=%m %f"
 
 -- Check if the system is Windows and set PowerShell as the default shell
 if vim.loop.os_uname().sysname == "Windows_NT" then
