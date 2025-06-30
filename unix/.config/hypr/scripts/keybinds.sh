1f8e4fe2b8d144ae5cdae8d77a285cf70476f064
Carlos Castro <carloscastro7@gmail.com>
2025-06-30 12:19:35 +0100

    add keybinds rofi menu;

---
+++ unix/.config/hypr/scripts/keybinds.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+HYPR_CONF="$HOME/.config/hypr/hyprland.conf"
+
+# extract the keybinding from hyprland.conf
+mapfile -t BINDINGS < <(grep '^bind' "$HYPR_CONF")
+
+CHOICE=$(printf '%s\n' "${BINDINGS[@]}" | rofi -dmenu -i -markup-rows -p "Hyprland Keybinds:" -theme "$HOME/.config/rofi/config.rasi")
+
+# extract cmd from span <span color='gray'>cmd</span>
+CMD=$(echo "$CHOICE" | sed -n 's/.*<span color='\''gray'\''>\(.*\)<\/span>.*/\1/p')
+
+# execute it if first word is exec else use hyprctl dispatch
+if [[ $CMD == exec* ]]; then
+  eval "$CMD"
+else
+  hyprctl dispatch "$CMD"
+fi
