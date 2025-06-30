324f80952645d148482e969eb8ca6bbbca40a248
Carlos Castro <carloscastro7@gmail.com>
2025-06-30 14:00:37 +0100

    update keybinds script;

--- unix/.config/hypr/scripts/keybinds.sh
+++ unix/.config/hypr/scripts/keybinds.sh
@@ -3,8 +3,9 @@
 HYPR_CONF="$HOME/.config/hypr/hyprland.conf"
 
 # extract the keybinding from hyprland.conf
-mapfile -t BINDINGS < <(grep '^bind' "$HYPR_CONF")
-
+mapfile -t BINDINGS < <(grep '^bind = ' "$HYPR_CONF" |
+  sed -e 's/  */ /g' -e 's/bind = //g' -e 's/, /,/g' -e 's/ # /,/' |
+  awk -F, -v q="'" '{cmd=""; for(i=3;i<NF;i++) cmd=cmd $(i) " ";print "<b>"$1 " + " $2 "</b> -> <i>" $NF " ,</i><span color=" q "gray" q ">" cmd "</span>"}')
 CHOICE=$(printf '%s\n' "${BINDINGS[@]}" | rofi -dmenu -i -markup-rows -p "Hyprland Keybinds:" -theme "$HOME/.config/rofi/config.rasi")
 
 # extract cmd from span <span color='gray'>cmd</span>
