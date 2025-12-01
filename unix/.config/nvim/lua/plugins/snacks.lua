cd2ed435f73c419b299d6a98107a6cdb3d3e3e26
Carlos Castro <carloscastro7@gmail.com>
2025-12-01 13:57:04 +0000

    update snacks.nvim layout;

--- unix/.config/nvim/lua/plugins/snacks.lua
+++ unix/.config/nvim/lua/plugins/snacks.lua
@@ -11,7 +11,7 @@ return {
                 layout = {
                     -- presets options : "default" , "ivy" , "ivy-split" , "telescope" , "vscode", "select" , "sidebar"
                     -- override picker layout in keymaps function as a param below
-                    preset = "ivy", -- defaults to this layout unless overidden
+                    preset = "default", -- defaults to this layout unless overidden
                 },
             },
         },
@@ -31,11 +31,11 @@ return {
             { "<leader>fg", function() Snacks.picker.git_files() end, desc = "Find Files (git-files)" },
             { "<leader>fR", LazyVim.pick("oldfiles"), desc = "Recent" },
             { "<leader>fr", function() Snacks.picker.recent({ filter = { cwd = true }}) end, desc = "Recent (cwd)" },
-            { "<leader>fp", function() Snacks.picker.projects({ layout = "default" }) end, desc = "Projects" },
+            { "<leader>fp", function() Snacks.picker.projects() end, desc = "Projects" },
             -- git
             { "<leader>gd", function() Snacks.picker.git_diff() end, desc = "Git Diff (hunks)" },
             { "<leader>gs", function() Snacks.picker.git_status() end, desc = "Git Status" },
-            { "<leader>gS", function() Snacks.picker.git_stash({ layout = "default" }) end, desc = "Git Stash" },
+            { "<leader>gS", function() Snacks.picker.git_stash() end, desc = "Git Stash" },
             -- Grep
             { "<leader>sb", function() Snacks.picker.lines() end, desc = "Buffer Lines" },
             { "<leader>sB", function() Snacks.picker.grep_buffers() end, desc = "Grep Open Buffers" },
@@ -45,26 +45,26 @@ return {
             { "<leader>sw", LazyVim.pick("grep_word"), desc = "Visual selection or word (Root Dir)", mode = { "n", "x" } },
             { "<leader>sW", LazyVim.pick("grep_word", { root = false }), desc = "Visual selection or word (cwd)", mode = { "n", "x" } },
             -- search
-            { '<leader>s"', function() Snacks.picker.registers({ layout = "default" }) end, desc = "Registers" },
+            { '<leader>s"', function() Snacks.picker.registers() end, desc = "Registers" },
             { '<leader>s/', function() Snacks.picker.search_history() end, desc = "Search History" },
-            { "<leader>sa", function() Snacks.picker.autocmds({ layout = "default" }) end, desc = "Autocmds" },
+            { "<leader>sa", function() Snacks.picker.autocmds() end, desc = "Autocmds" },
             { "<leader>sc", function() Snacks.picker.command_history() end, desc = "Command History" },
-            { "<leader>sC", function() Snacks.picker.commands({ layout = "default" }) end, desc = "Commands" },
+            { "<leader>sC", function() Snacks.picker.commands() end, desc = "Commands" },
             { "<leader>sd", function() Snacks.picker.diagnostics() end, desc = "Diagnostics" },
             { "<leader>sD", function() Snacks.picker.diagnostics_buffer() end, desc = "Buffer Diagnostics" },
-            { "<leader>sh", function() Snacks.picker.help({ layout = "default" }) end, desc = "Help Pages" },
-            { "<leader>sH", function() Snacks.picker.highlights({ layout = "default" }) end, desc = "Highlights" },
-            { "<leader>si", function() Snacks.picker.icons({ layout = "default" }) end, desc = "Icons" },
-            { "<leader>sj", function() Snacks.picker.jumps({ layout = "default" }) end, desc = "Jumps" },
-            { "<leader>sk", function() Snacks.picker.keymaps({ layout = "default" }) end, desc = "Keymaps" },
-            { "<leader>sl", function() Snacks.picker.loclist({ layout = "default" }) end, desc = "Location List" },
-            { "<leader>sM", function() Snacks.picker.man({ layout = "default" }) end, desc = "Man Pages" },
-            { "<leader>sm", function() Snacks.picker.marks({ layout = "default" }) end, desc = "Marks" },
+            { "<leader>sh", function() Snacks.picker.help() end, desc = "Help Pages" },
+            { "<leader>sH", function() Snacks.picker.highlights() end, desc = "Highlights" },
+            { "<leader>si", function() Snacks.picker.icons() end, desc = "Icons" },
+            { "<leader>sj", function() Snacks.picker.jumps() end, desc = "Jumps" },
+            { "<leader>sk", function() Snacks.picker.keymaps() end, desc = "Keymaps" },
+            { "<leader>sl", function() Snacks.picker.loclist() end, desc = "Location List" },
+            { "<leader>sM", function() Snacks.picker.man() end, desc = "Man Pages" },
+            { "<leader>sm", function() Snacks.picker.marks() end, desc = "Marks" },
             { "<leader>sR", function() Snacks.picker.resume() end, desc = "Resume" },
             { "<leader>sq", function() Snacks.picker.qflist() end, desc = "Quickfix List" },
             { "<leader>su", function() Snacks.picker.undo() end, desc = "Undotree" },
             -- ui
-            { "<leader>uC", function() Snacks.picker.colorschemes({ layout = "default" }) end, desc = "Colorschemes" },
+            { "<leader>uC", function() Snacks.picker.colorschemes() end, desc = "Colorschemes" },
         },
     },
 }
